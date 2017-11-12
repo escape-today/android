4f8de3bec42c35077ae6964ecce072188a430387
Louis Thurston-Gibson <luigigib@hotmail.co.uk>
2017-11-12 03:32:21 +0000

    Initial Commit

--- app/src/main/java/domain/escapetodayclient/MainActivity.java
+++ app/src/main/java/domain/escapetodayclient/MainActivity.java
@@ -32,13 +32,13 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(context,"clicked: " + item.getTitle(), Toast.LENGTH_SHORT).show();
             switch (item.getItemId()){
                 case R.id.menu_main_about:
-                    //showAbout();
+                    showAbout();
                     return true;
                 case R.id.menu_main_new_scenario:
                     showNewScenario();
                     return true;
                 case R.id.menu_main_help:
-                    //showHelp();
+                    showHelp();
                     return true;
                 case R.id.menu_main_customise_scenario:
                     //showCustomise();
@@ -47,9 +47,20 @@ public class MainActivity extends AppCompatActivity {
             }
         }
 
+        private void showAbout(){
+            Intent i = new Intent(context, AboutActivity.class);
+            startActivity(i);
+        }
+
         private void showNewScenario(){
             Intent i = new Intent(context, NewScenarioActivity.class);
             startActivity(i);
         }
+
+        private void showHelp(){
+            Intent i = new Intent(context, HelpActivity.class);
+            startActivity(i);
+        }
+
     }
 }
