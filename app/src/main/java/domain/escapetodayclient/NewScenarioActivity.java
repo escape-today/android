3c8a5bc9ae332a57d53b45c76269963c40f1002d
Jonathan Cowling <jonathan.cowling@student.manchester.ac.uk>
2017-11-12 05:21:42 +0000

    Added a service to fetch the current users location name

--- app/src/main/java/domain/escapetodayclient/NewScenarioActivity.java
+++ app/src/main/java/domain/escapetodayclient/NewScenarioActivity.java
@@ -1,5 +1,11 @@
 package domain.escapetodayclient;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -11,10 +17,28 @@ import android.widget.Adapter;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+import org.json.JSONArray;
+
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.logging.Logger;
 
 public class NewScenarioActivity extends AppCompatActivity {
+
+    JSONArray jsonData = new JSONArray();
+    //String locationString = getLocation();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,11 +52,28 @@ public class NewScenarioActivity extends AppCompatActivity {
         rv.setLayoutManager(lm);
         rv.setAdapter(mAdapter);
 
+        RequestQueue queue = Volley.newRequestQueue(this);
+        JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, getString(R.string.url_get_flights), null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray res) {
+                        Toast.makeText(getApplicationContext(), "done request", Toast.LENGTH_SHORT).show();
+                        jsonData = res;
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Logger.getLogger("hello").warning(error.getMessage());
+            }
+        });
+
+        queue.add(request);
+
         // redundant
         mAdapter.notifyDataSetChanged();
     }
 
-    private class ScenarioItemAdapter extends RecyclerView.Adapter<ScenarioItemAdapter.ScenarioItemViewHolder>{
+    private class ScenarioItemAdapter extends RecyclerView.Adapter<ScenarioItemAdapter.ScenarioItemViewHolder> {
 
         @Override
         public ScenarioItemViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
@@ -55,7 +96,7 @@ public class NewScenarioActivity extends AppCompatActivity {
         @Override
         public int getItemCount() {
             // data.size()
-            return 1000000;
+            return 100000; //jsonData.length();
         }
 
         protected class ScenarioItemViewHolder extends RecyclerView.ViewHolder {
@@ -68,5 +109,37 @@ public class NewScenarioActivity extends AppCompatActivity {
                 title = itemView.findViewById(R.id.scenario_item_title);
             }
         }
+    }
+
+    private String getLocation() {
+        FusedLocationProviderClient mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return "NULL";
+        }
+
+        final Context context = this;
+
+        mFusedLocationClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+
+
+            @Override
+            public void onSuccess(Location location) {
+                if (location != null) {
+                    Intent i = new Intent(context, FetchAddressIntentService.class);
+                    //i.putExtra(location.getLongitude());
+                    startActivity(i);
+                }
+            }
+        });
+
+        return "";
     }
 }
